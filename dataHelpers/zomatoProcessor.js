afb32a952bbc6356aec270118a1601231fd3d5ce
Won Seob Shin <wonseob07@gmail.com>
2019-06-21 22:04:12 +0000

    fixed zomato to resolve punctuations and special characters

--- dataHelpers/zomatoProcessor.js
+++ dataHelpers/zomatoProcessor.js
@@ -3,9 +3,13 @@ const apiKey = process.env.zomatoAPI;
 
 const https = require('https');
 
+function normalizeName(name) {
+  return name.toLowerCase().split('&').join('').split('\'').join('');
+}
+
 module.exports = (zomatoSearch) => {
   return new Promise((resolve) => {
-    const query = encodeURIComponent(zomatoSearch); //.split(' ').join('%20').split('&').join('%26').split('');
+    const query = encodeURIComponent(normalizeName(zomatoSearch));
     https.get(`https://developers.zomato.com/api/v2.1/search?q=${query}&lat=49.2811&lon=-123.1149&radius=100000&apikey=${apiKey}`, (res) => {
       let data = '';
 
@@ -17,24 +21,23 @@ module.exports = (zomatoSearch) => {
         console.log("search complete!");
         const result = JSON.parse(data);
 
-        if (result.results_found !== 0 && result.results_found
- !== undefined) {
-          result.restaurants.forEach((element) => {
+        if (result.results_found !== 0 && result.results_found !== undefined) {
+          for (const element of result.restaurants) {
             console.log("FOUND! I'm now searching for name matches...");
-            console.log(element.restaurant.name.toLowerCase().split('\'').join(''));
-            if (element.restaurant.name.toLowerCase().split('\'').join('').includes(zomatoSearch.toLowerCase())) {
+            console.log("result: ", normalizeName(element.restaurant.name));
+            console.log("search: ", normalizeName(zomatoSearch));
+            if (normalizeName(element.restaurant.name).includes(normalizeName(zomatoSearch))) {
               // return element;
 
               console.log("name match found! resolving 'restaurants' as category");
               resolve(['Restaurant']);
-            } else {
-              resolve(false);
-            }
-          });
+              return;
+            };
+          }
           // console.log(restaurant);
-        } else {
-          resolve(false);
         }
+
+        resolve(false);
       });
     }).on("error", (err) => {
       console.error('error: ', err);
