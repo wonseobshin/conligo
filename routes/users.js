37139e3642ab47a7e0fa7aa713389afc62d0e9f3
Steven Wing <steven.wing82@gmail.com>
2019-06-24 08:53:38 -0700

    fix response error on deleting items

--- routes/users.js
+++ routes/users.js
@@ -39,12 +39,13 @@ module.exports = (knex) => {
     }
   })
 
-  router.delete("/item/:itemID", (req) => {
+  router.delete("/item/:itemID", (req, res) => {
     const itemID = Number(req.params.itemID);
     knex('todos')
       .where('id', itemID)
       .del()
       .then(() => {
+        res.status(202).json({});
       }).catch((error) => {
         console.error(error);
       })
